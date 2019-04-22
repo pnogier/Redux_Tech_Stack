4a5d7e6942fa5dc71a3209f5b442cc582e2a6713
pnogier <pnogiercontact@gmail.com>
2019-04-22 22:11:03 +0200

    :construction: Rendering a list of items

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-// Import
-import React, { Component } from 'react';
-import { Text, View, StatusBar, Platform } from 'react-native';
+import React from 'react';
+import { View, StatusBar, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-// Local Import
+import reducers from './reducers';
+import { Header } from './components/Common';
+import LibraryList from './components/LibraryList';
 
-class App extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View
-          style={{
-            backgroundColor: '#F8F8F8',
-            height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
-            }}
-        >
-          <StatusBar
-            translucent
-            backgroundColor="#F8F8F8"
-            barStyle="dark-content"
-          />
-        </View>
-        <View style={{ flex: 1 }}>
-          <Text>
-            App
-          </Text>
-        </View>
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: '#F8F8F8',
+          height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
+          }}
+      >
+        <StatusBar
+          translucent
+          backgroundColor="#F8F8F8"
+          barStyle="dark-content"
+        />
       </View>
-    );
-  }
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    </View>
+  );
 };
 
 export default App;
